014b3c97e7f6c82f60db0f8f802c0a3545a9d9bb
GugaToca <gustavotocalino08@outlook.com>
2025-04-28 19:00:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,18 @@ links.forEach(link => {
     navLinks.classList.remove('active');
   });
 });
+
+// Aviso de Cookies
+document.addEventListener('DOMContentLoaded', function () {
+  const cookieBanner = document.getElementById('cookie-banner');
+  const acceptCookies = document.getElementById('accept-cookies');
+
+  if (!localStorage.getItem('cookiesAccepted')) {
+    cookieBanner.style.display = 'flex';
+  }
+
+  acceptCookies.addEventListener('click', function () {
+    localStorage.setItem('cookiesAccepted', 'true');
+    cookieBanner.style.display = 'none';
+  });
+});
